4047d885acad9d62cceab0e70557be8faf634ee0
Aureo151 <aureotzic@gmail.com>
2025-04-07 09:21:55 -0600

    inscripciones activo

--- Simulacro Parcial/FormInscripcion.Designer.cs
+++ Simulacro Parcial/FormInscripcion.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbdpi = new System.Windows.Forms.ComboBox();
+            this.cmbNombreTaller = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DPI";
+            // 
+            // cmbdpi
+            // 
+            this.cmbdpi.FormattingEnabled = true;
+            this.cmbdpi.Location = new System.Drawing.Point(139, 13);
+            this.cmbdpi.Name = "cmbdpi";
+            this.cmbdpi.Size = new System.Drawing.Size(211, 24);
+            this.cmbdpi.TabIndex = 1;
+            // 
+            // cmbNombreTaller
+            // 
+            this.cmbNombreTaller.FormattingEnabled = true;
+            this.cmbNombreTaller.Location = new System.Drawing.Point(139, 79);
+            this.cmbNombreTaller.Name = "cmbNombreTaller";
+            this.cmbNombreTaller.Size = new System.Drawing.Size(211, 24);
+            this.cmbNombreTaller.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre Taller";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(20, 137);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(138, 64);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(185, 137);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(138, 64);
+            this.btnMostrar.TabIndex = 5;
+            this.btnMostrar.Text = "Actualizar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(384, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(404, 227);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // FormInscripcion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 269);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.cmbNombreTaller);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbdpi);
+            this.Controls.Add(this.label1);
+            this.Name = "FormInscripcion";
             this.Text = "FormInscripcion";
+            this.Load += new System.EventHandler(this.FormInscripcion_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbdpi;
+        private System.Windows.Forms.ComboBox cmbNombreTaller;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Simulacro Parcial/FormInscripcion.cs
+++ Simulacro Parcial/FormInscripcion.cs
@@ -12,9 +12,55 @@ namespace Simulacro_Parcial
 {
     public partial class FormInscripcion : Form
     {
+        List<Estudiante> estudiantes = new List<Estudiante>();
+        List<Taller> talleres = new List<Taller>();
+        List<Inscripcion> inscripciones = new List<Inscripcion>();
         public FormInscripcion()
         {
             InitializeComponent();
+        }
+
+        private void FormInscripcion_Load(object sender, EventArgs e)
+        {
+            EstudianteArchivo estudianteArchivo = new EstudianteArchivo();
+            estudiantes = estudianteArchivo.Leer("../../Estudiantes.json");
+
+            cmbdpi.DataSource = estudiantes;
+            cmbdpi.ValueMember = "dpi";
+            cmbdpi.DisplayMember = "dpi";
+
+            TallerArchivo tallerArchivo = new TallerArchivo();
+            talleres = tallerArchivo.Leer("../../Talleres.json");
+
+            cmbNombreTaller.DataSource = talleres;
+            cmbNombreTaller.ValueMember = "nombre_taller";
+            cmbNombreTaller.DisplayMember = "nombre_taller";
+        }
+        private void mostrar()
+        {
+            InscripcionArchivo inscripcionArchivo = new InscripcionArchivo();
+            inscripciones = inscripcionArchivo.Leer("../../Inscripciones.json");
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = inscripciones;
+            dataGridView1.Refresh();
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Inscripcion inscripcion = new Inscripcion();
+            inscripcion.dpi = cmbdpi.SelectedValue.ToString();
+            inscripcion.nombre_taller = cmbNombreTaller.SelectedValue.ToString();
+            inscripcion.fecha = DateTime.Now;
+
+            inscripciones.Add(inscripcion);
+
+            InscripcionArchivo inscripcionArchivo = new InscripcionArchivo();
+            inscripcionArchivo.Guardar("../../Inscripciones.json", inscripciones);
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            mostrar();
         }
     }
 }
