25558263a6ffaa57d81ae887771098c0dae06aae
Aureo151 <aureotzic@gmail.com>
2025-04-07 09:06:19 -0600

    talleres activo

--- Simulacro Parcial/FormTaller.Designer.cs
+++ Simulacro Parcial/FormTaller.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombreTaller = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedtxtCosto = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(177, 23);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(169, 22);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtNombreTaller
+            // 
+            this.txtNombreTaller.Location = new System.Drawing.Point(177, 88);
+            this.txtNombreTaller.Name = "txtNombreTaller";
+            this.txtNombreTaller.Size = new System.Drawing.Size(169, 22);
+            this.txtNombreTaller.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre Taller";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Costo";
+            // 
+            // maskedtxtCosto
+            // 
+            this.maskedtxtCosto.Location = new System.Drawing.Point(177, 151);
+            this.maskedtxtCosto.Mask = "999.99";
+            this.maskedtxtCosto.Name = "maskedtxtCosto";
+            this.maskedtxtCosto.Size = new System.Drawing.Size(169, 22);
+            this.maskedtxtCosto.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 54);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(380, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(394, 243);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(189, 212);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(157, 54);
+            this.btnMostrar.TabIndex = 8;
+            this.btnMostrar.Text = "Actualizar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // FormTaller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 290);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedtxtCosto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombreTaller);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label1);
+            this.Name = "FormTaller";
             this.Text = "FormTaller";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombreTaller;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedtxtCosto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnMostrar;
     }
 }
--- Simulacro Parcial/FormTaller.cs
+++ Simulacro Parcial/FormTaller.cs
@@ -12,9 +12,42 @@ namespace Simulacro_Parcial
 {
     public partial class FormTaller : Form
     {
+        List<Taller> talleres = new List<Taller>();
         public FormTaller()
         {
             InitializeComponent();
+        }
+
+        private void mostrar()
+        {
+            TallerArchivo tallerArchivo = new TallerArchivo();
+            talleres = tallerArchivo.Leer("../../Talleres.json");
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = talleres; 
+            dataGridView1.Refresh();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Taller taller = new Taller();
+            taller.codigo = txtCodigo.Text;
+            taller.nombre_taller = txtNombreTaller.Text;
+            taller.costo = Convert.ToDecimal(maskedtxtCosto.Text);
+
+            talleres.Add(taller);
+
+            TallerArchivo tallerArchivo = new TallerArchivo();
+            tallerArchivo.Guardar("../../Talleres.json", talleres);
+
+            txtCodigo.Clear();
+            txtNombreTaller.Clear();
+            maskedtxtCosto.Clear();
+
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            mostrar();
         }
     }
 }
